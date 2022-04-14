d8c2f12e5b1de11563c0bd3dcd448c504c8b0e3c
drupchen <hhdrupchen@gmail.com>
2022-04-14 22:32:54 +0100

    put dict icon on top + add 3rd page

--- dictify.py
+++ dictify.py
@@ -2,17 +2,43 @@ from collections import defaultdict
 from pathlib import Path
 
 import yaml
-from openpyxl import Workbook
-from openpyxl.styles import Font, Alignment
-from openpyxl.utils import get_column_letter
+import json
 import pyewts
 
 conv = pyewts.pyewts()
 
 
+def dictify_text(string, selection_yaml='data/dictionaries/dict_cats.yaml'):
+    """
+    takes segmented text and finds entries from dictionaries
+    :param selection_yaml: add None or "" to prevent selection
+    :param string: segmented text to be processed
+    :return: list of tuples containing the word and a dict containing the definitions(selected or not) and an url
+    """
+    string = string.replace('\n', ' \n ')
+    words = []
+    for w in string.split(' '):
+        if w:
+            words.append((w, {}))
+
+    dicts = load_dicts()
+    for num, word in enumerate(words):
+        lemma = word[0].rstrip('་')
+        defs = dicts[lemma]
+        # filter
+        if selection_yaml:
+            defs = select_defs(defs, yaml_path=selection_yaml)
+        words[num][1]['defs'] = defs
+        # url
+        url = gen_link(lemma)
+        words[num][1]['url'] = url
+
+    return words
+
+
 def load_dicts():
     dicts = defaultdict(dict)
-    dict_path = Path('data/dictionaries/converted')
+    dict_path = Path(__file__).parent / 'data/dictionaries/converted'
     for f in sorted(dict_path.glob('*.txt')):
         name = f.stem
         lines = f.read_text().split('\n')
@@ -26,91 +52,24 @@ def load_dicts():
     return dicts
 
 
-def dictify_text(string):
-    string = string.replace('\n', ' ')
-    words = []
-    for w in string.split(' '):
-        words.append([w, {}])
-
-    dicts = load_dicts()
-    for num, word in enumerate(words):
-        lemma = word[0].rstrip('་')
-        defs = dicts[lemma]
-        # filter
-        words[num][1]['defs'] = defs
-
-    for num, word in enumerate(words):
-        if num <= 0:
-            continue
-        if num >= len(words):
-            continue
-
-        prev_lemma = words[num-1][0]
-        cur_lemma = words[num][0]
-        if cur_lemma.startswith('-'):
-            words[num-1][0] = prev_lemma[:-1]
-            words[num][0] = cur_lemma[1:]
-        if cur_lemma.endswith('།'):
-            if prev_lemma[-2] != 'ང' and prev_lemma.endswith('་'):
-                words[num-1][0] = prev_lemma[:-1]
-            words[num][0] += ' '
-
-    return words
-
-
-def generate_xlsx(title, content, out_file):
-    font = 'Jomolhari'
-    lemma_style = Font(font, size=18, bold=True)
-    tib_style = Font(font, size=15)
-    alignmnt = Alignment(horizontal="left", vertical="distributed", wrap_text=True)
-
-    def fill_cell(sheet, row, col, string, font_style, height=None, width=None, link=None):
-        sheet.cell(row=row, column=col).value = string
-        sheet.cell(row=row, column=col).font = font_style
-        sheet.cell(row=row, column=col).alignment = alignmnt
-        if height:
-            sheet.row_dimensions[row].height = height
-        if width:
-            sheet.column_dimensions[get_column_letter(col)].width = width
-        if link:
-            sheet.cell(row=row, column=col).hyperlink = link
-
-    wb = Workbook()
-    wb.remove(wb['Sheet'])
-    ws = wb.create_sheet(title=title)
-    row, col = 1, 1
-    width, height = 50, 50
-    for word, data in content:
-        defs = select_defs(data['defs'])
-        if defs:
-            link = gen_link(word)
-            fill_cell(ws, row, col, word, lemma_style, height=height, link=link)
-            col += 1
-            defs = defs.split('\n\n')
-            for d in defs:
-                fill_cell(ws, row, col, d, tib_style, width=width)
-                col += 1
-        else:
-            fill_cell(ws, row, col, word, lemma_style, height=height)
-            col += 1
-        row += 1
-        col = 1
-
-    wb.save(out_file)
-
-
-def select_defs(defs):
-    cats = yaml.safe_load(Path('data/dictionaries/dict_cats.yaml').read_text())
+def select_defs(defs, yaml_path):
+    yaml_path = Path(__file__).parent / yaml_path
+    cats = yaml.safe_load(yaml_path.read_text())
     english, tibetan = cats['english']['dictionary'], cats['tibetan']['dictionary']
+    tibetan += cats['tibetan']['tenses']
+    tibetan += cats['tibetan']['definitions_bo']
+    tibetan += cats['tibetan']['divisions_bo']
+    tibetan += cats['tibetan']['synonyms_bo']
+    tibetan += cats['tibetan']['examples_bo']
 
     selected = {}
-    # selecting English definition
+    # selecting the first English definition from the list in dict_cats.yaml
     for full, name in english:
         if full in defs:
             selected['en'] = (name, defs[full])
             break
 
-    # selecting Tibetan definition
+    # selecting the first Tibetan definition from the list in dict_cats.yaml
     for full, name in tibetan:
         if full in defs:
             selected['bo'] = (name, defs[full])
@@ -118,11 +77,11 @@ def select_defs(defs):
 
     # format selected
     if 'en' in selected and 'bo' in selected:
-        return f"{selected['en'][0]}\n{selected['en'][1]}\n\n{selected['bo'][0]}\n{selected['bo'][1]}"
+        return {'en': [selected['en'][0],selected['en'][1]], 'bo': [selected['bo'][0], selected['bo'][1]]}
     elif 'en' in selected:
-        return f"{selected['en'][0]}\n{selected['en'][1]}"
+        return {'en': [selected['en'][0], selected['en'][1]]}
     elif 'bo' in selected:
-        return f"{selected['bo'][0]}\n{selected['bo'][1]}"
+        return {'bo': [selected['bo'][0], selected['bo'][1]]}
     else:
         return None
 
@@ -135,50 +94,11 @@ def gen_link(word):
     return link_pattern.format(word=wylie)
 
 
-def generate_html(title, content, out_file):
-    html_pattern = '<!doctype html><meta charset=utf-8><title>{title}</title>\n<html lang=en>\n' \
-                '<head>\n<meta charset=utf-8>\n<title>{title}</title>\n</head><style>\n{style}\n</style>\n' \
-                   '<body>\n<p>\n{body}\n</p>\n</body>\n</html>'
-    entry_pattern = '<a href="{link}" target="_blank" title="{defs}">{word}</a>'
-    style = """a:link {
-  text-decoration: none;
-  color: black;
-}
-
-a:visited {
-  color: gray;
-  text-decoration: none;
-  display: inline-block;
-}
-
-a:hover {
-  background-color: yellow;
-}"""
-    font_size = 'x-large'  # large, larger, medium, small, smaller, x-large, xx-large, xx-small, or "0cm" -> change value
-    style += "p {\n    font-size: " + font_size + ";\n}"
-
-    body = []
-    for word, data in content:
-        defnts = data['defs']
-        if defnts:
-            defs = select_defs(defnts)
-            link = gen_link(word)
-            def_out = entry_pattern.format(link=link, word=word, defs=defs)
-            body.append(def_out)
-        else:
-            body.append(word)
-
-    out = html_pattern.format(title=title, style=style, body=''.join(body))
-
-    Path(out_file).write_text(out)
-
-
-def batch_process(in_path):
-    for f in Path(in_path).glob('*.txt'):
-        dump = f.read_text()
+if __name__ == '__main__':
+    for f in Path('input').glob('*.txt'):
+        dump = f.read_text(encoding='utf-8')
         out = dictify_text(dump)
-        generate_html(f.stem, out, Path('output') / (f.stem + '.html'))
-        generate_xlsx(f.stem, out, Path('output') / (f.stem + '.xlsx'))
+        out_f = Path('output') / f.name
+        out_f.write_text(json.dumps(out, ensure_ascii=False, indent=4))
 
-
-batch_process('input')
+__all__ = [dictify_text]
